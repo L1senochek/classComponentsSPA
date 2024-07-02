049f887b4b6844c5c5a0c09f5820907d8207b063
l1senochek <lisenochek96@gmail.com>
2024-07-02 17:34:11 +0300

    feat: add Header & headerImg

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './header.module.css';
+
+class Header extends React.Component {
+  render() {
+    const headerImg =
+      'https://assets.turbologo.ru/assets/landing/watermark-349102dbc1cb51b5b93f17117681dccfcccdec93ef6fab3832f9ddaf532cb72d.png';
+
+    return (
+      <header className={styles.header}>
+        <img src={headerImg} alt="Logo" />
+        <select>
+          <option value="ru">RU</option>
+          <option value="en">EN</option>
+        </select>
+        Watch
+      </header>
+    );
+  }
+}
+
+export default Header;
